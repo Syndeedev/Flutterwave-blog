f51990a4b0fd5d42c786501bf323d73a0e60f390
cynthia <cynthia@seomonitor.com>
2022-11-25 11:35:05 +0100

    project setup

---
+++ components/BlogListing.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Optimizing CSS for faster page loads</h1>
+</template>
+
+<script>
+export default {
+  name: "BlogListing",
+};
+</script>
+
+<style></style>
---
+++ layouts/error.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Page not found</h1>
+    <NuxtLink to="/">Back to Home page</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "error",
+};
+</script>
