5fa18d062b8701abe5352c8ac92625b18ef94aee
cynthia <cynthia@seomonitor.com>
2022-11-26 23:04:49 +0100

    Refactoring

--- pages/_id.vue
+++ pages/_id.vue
@@ -1,25 +1,29 @@
 <template>
-  <div class="blog__detail" v-if="currentPost">
-    <div class="blog__content">
-      <div class="card__header">
-        <div class="card__header_top">
-          <span class="card__header_time_posted">
-            By {{ currentPost.author }}
-          </span>
-          <div class="dot"></div>
-          <span class="card__header_time_posted">
-            {{ $moment(currentPost.publishedDate).fromNow() }}
-          </span>
-        </div>
-        <h3 class="card__title" v-html="currentPost.title"></h3>
-      </div>
+  <div>
+    <loader v-if="loading" />
 
-      <p v-html="currentPost.content"></p>
-    </div>
-    <div v-if="randomPosts.length">
-      <h4>More Articles</h4>
-      <div class="cards_container">
-        <post-card v-for="post in randomPosts" :key="post.id" :post="post" />
+    <div class="blog__detail" v-else>
+      <div class="blog__content">
+        <div class="card__header">
+          <div class="card__header_top">
+            <span class="card__header_time_posted">
+              By {{ currentPost.author }}
+            </span>
+            <div class="dot"></div>
+            <span class="card__header_time_posted">
+              {{ $moment(currentPost.publishedDate).fromNow() }}
+            </span>
+          </div>
+          <h3 class="card__title" v-html="currentPost.title"></h3>
+        </div>
+
+        <p v-html="currentPost.content"></p>
+      </div>
+      <div v-if="randomPosts.length">
+        <h4>More Articles</h4>
+        <div class="cards_container">
+          <post-card v-for="post in randomPosts" :key="post.id" :post="post" />
+        </div>
       </div>
     </div>
   </div>
@@ -29,18 +33,32 @@
 import { mapGetters } from "vuex";
 export default {
   name: "BlogDetails",
-
+  data() {
+    return {
+      loading: true,
+    };
+  },
   computed: {
-    ...mapGetters(["allPosts"]),
-    currentPost() {
-      return this.allPosts.find((post) => post.slug === this.$route.params.id);
-    },
+    ...mapGetters(["allPosts", "currentPost"]),
     randomPosts() {
       return [...this.allPosts]
         .filter((post) => post.id !== this.currentPost.id)
         .sort(() => Math.random() - Math.random())
         .slice(0, 3);
     },
+  },
+  mounted() {
+    this.$store
+      .dispatch("getCurrentPost", this.$route.params.id)
+      .then((res) => {
+        if (res) {
+          this.loading = false;
+        } else {
+          return this.$nuxt.error({
+            statusCode: 404,
+          });
+        }
+      });
   },
 };
 </script>
